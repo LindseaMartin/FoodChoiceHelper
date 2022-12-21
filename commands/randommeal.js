89cd84b639d23925ca143ad3ba770b243a5bbe76
LindseaMartin <ninzerb@gmail.com>
2022-12-21 12:21:12 -0700

    back

--- commands/randommeal.js
+++ commands/randommeal.js
@@ -1,5 +1,11 @@
 const { request } = require("undici");
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,21 +14,36 @@ module.exports = {
 
   async execute(interaction) {
     const mealResult = await request(
-      'https://www.themealdb.com/api/json/v1/1/random.php'
+      "https://www.themealdb.com/api/json/v1/1/random.php"
     );
 
-      const { meals } = await mealResult.body.json();
-      if (!meals.length) {
-        return interaction.editReply("No meals found!");
-      }
+    const { meals } = await mealResult.body.json();
+    if (!meals.length) {
+      return interaction.editReply("No meals found!");
+    }
 
     const [answer] = meals;
+
+    const row = new ActionRowBuilder()
+    .addComponents(
+      new ButtonBuilder()
+      .setLabel("Yes")
+      .setStyle(ButtonStyle.Success)
+      // new ButtonBuilder().setCustomId("No")
+      // .setLabel("No")
+      // .setStyle(ButtonStyle.red)*/
+    );
 
     const embed = new EmbedBuilder()
       .setColor(0xefff00)
       .setTitle(answer.strMeal)
-      .setURL(answer.strSource);
+      .setURL(answer.strSource)
+      .setImage(`${answer.strMealThumb}`)
+      .addFields({ name: "Category", value: answer.strCategory });
 
-    interaction.editReply({ embeds: [embed] });
+    await interaction.editReply({
+      embeds: [embed],
+      components: [row]
+    });
   },
 };
